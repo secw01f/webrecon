b2ac1b0e74ea3f7260eac3fcb04fc3e48857685d
secw01f <secw01f@kali.localdomain>
2020-04-21 11:16:24 -0400

    Fixed installation issues

--- setup.sh
+++ setup.sh
@@ -2,25 +2,17 @@
 
 sudo pip install -r requirements.txt
 
-if [ nmap ]; then
-  echo "[ + ] Nmap already installed"
-else
-  echo "[ ! ] Installing nmap"
-  sudo apt-get install nmap
-  echo "[ + ] Nmap installation complete"
-fi
+echo "[ ! ] Installing nmap"
+sudo apt-get install nmap
+echo "[ + ] Nmap installation complete"
 
-if [ go ]; then
-  echo "[ + ] Go already installed"
-else
-  echo "[ ! ] Installing go"
-  cd ~/Downloads && wget https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz
-  tar -C /usr/local -xzf go1.14.2.linux-amd64.tar.gz
-  echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-  source ~/.bashrc
-  cd ~
-  echo "[ + ] Go installation complete"
-fi
+echo "[ ! ] Installing go"
+cd ~/Downloads && wget https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.14.2.linux-amd64.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+source ~/.bashrc
+cd ~
+echo "[ + ] Go installation complete"
 
 echo "[ ! ] Installing assetfinder, httprobe, meg, gf, and aquatone"
 go get github.com/tomnomnom/assetfinder
@@ -30,12 +22,8 @@ go get github.com/tomnomnom/gf
 go get github.com/michenriksen/aquatone
 echo "[ + ] Assetfinder, httprobe, meg, gf, and aquatone installation complete"
 
-if [ searchsploit ]; then
-  echo "[ + ] Searchsploit already installed"
-else
-  echo "[ ! ] Installing searchsploit"
-  sudo apt -y install exploitdb
-  echo "[ + ] Searchsploit installation complete"
-fi
+echo "[ ! ] Installing searchsploit"
+sudo apt -y install exploitdb
+echo "[ + ] Searchsploit installation complete"
 
 echo "[ + ] All installations complete"
