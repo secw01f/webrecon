190f2a45eecb032e42cb627f03004c820fcdb70c
secw01f <info@burkinfotech.com>
2020-12-05 13:40:14 -0500

    added zsh update

--- setup.sh
+++ setup.sh
@@ -10,9 +10,21 @@ echo "[ + ] Nmap installation complete"
 echo "[ ! ] Installing go"
 cd ~/Downloads && wget https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.14.2.linux-amd64.tar.gz
-echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
-echo "export PATH=\$PATH:~/go/bin" >> ~/.bashrc
-source ~/.bashrc
+
+if test -f ~/.bashrc;
+then
+  echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
+  echo "export PATH=\$PATH:~/go/bin" >> ~/.bashrc
+  source ~/.bashrc
+fi
+
+if test -f ~/.zshrc;
+then
+  echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.zshrc
+  echo "export PATH=\$PATH:~/go/bin" >> ~/.zshrc
+  source ~/.zshrc
+fi
+
 cd ~
 echo "[ + ] Go installation complete"
 
@@ -22,8 +34,19 @@ go get github.com/tomnomnom/httprobe
 go get github.com/tomnomnom/meg
 go get github.com/tomnomnom/gf
 go get github.com/michenriksen/aquatone
-echo "source ~/go/src/github.com/tomnomnom/gf/gf-completion.bash" >> ~/.bashrc
-source ~/.bashrc
+
+if test -f ~/.bashrc;
+then
+  echo "source ~/go/src/github.com/tomnomnom/gf/gf-completion.bash" >> ~/.bashrc
+  source ~/.bashrc
+fi
+
+if test -f ~/.zshrc;
+then
+  echo "source ~/go/src/github.com/tomnomnom/gf/gf-completion.bash" >> ~/.zshrc
+  source ~/.zshrc
+fi
+
 cp -r ~/go/src/github.com/tomnomnom/gf/examples ~/.gf
 echo "[ + ] Assetfinder, httprobe, meg, gf, and aquatone installation complete"
 
